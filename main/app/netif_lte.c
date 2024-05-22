c81542781f6c232251aa8f6bd38b06c304cf63e9
Yilin Sun <imi415@imi.moe>
2024-05-22 18:56:40 +0800

    Added LTE status task.
    Signed-off-by: Yilin Sun <imi415@imi.moe>

--- main/app/netif_lte.c
+++ main/app/netif_lte.c
@@ -19,40 +19,33 @@
 #define LTE_DTR_PIN CONFIG_APP_NETIF_LTE_DTR_GPIO
 #define LTE_APN     CONFIG_APP_NETIF_LTE_APN
 
-static const char *LOG_TAG = "A_LTE";
+static const char* LOG_TAG = "A_LTE";
 
+static QueueHandle_t s_lte_ppp_event_queue = NULL;
+
+static void app_netif_lte_pin_init(void);
 static void app_netif_lte_reset(void);
-static void app_netif_lte_enable(void);
-static void app_netif_lte_disable(void);
-static void app_netif_lte_ppp_event_cb(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void app_netif_lte_ppp_event_cb(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
+static void app_netif_lte_task(void* arguments);
 
 int app_netif_lte_init(void) {
     ESP_LOGI(LOG_TAG, "Initializing...");
 
-    gpio_config_t pin_conf = {
-        .pin_bit_mask = (1U << LTE_EN_PIN),
-        .intr_type    = GPIO_INTR_DISABLE,
-        .mode         = GPIO_MODE_OUTPUT_OD,
-        .pull_up_en   = GPIO_PULLUP_ENABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-    };
-
-    gpio_config(&pin_conf);
-
-    pin_conf.pin_bit_mask = 1U << LTE_DTR_PIN;
-    pin_conf.mode = GPIO_MODE_OUTPUT_OD;
-
-    gpio_config(&pin_conf);
-
-    gpio_set_level(LTE_DTR_PIN, 0U);
-
+    app_netif_lte_pin_init();
     app_netif_lte_reset();
+
+    s_lte_ppp_event_queue = xQueueCreate(4, sizeof(uint32_t));
+    if (s_lte_ppp_event_queue == NULL) {
+        ESP_LOGE(LOG_TAG, "Failed to create event queue.");
+
+        return -1;
+    }
 
     ESP_ERROR_CHECK(esp_event_handler_register(NETIF_PPP_STATUS, ESP_EVENT_ANY_ID, app_netif_lte_ppp_event_cb, NULL));
 
     esp_modem_dce_config_t dce_config       = ESP_MODEM_DCE_DEFAULT_CONFIG(LTE_APN);
     esp_netif_config_t     netif_ppp_config = ESP_NETIF_DEFAULT_PPP();
-    esp_netif_t           *lte_netif        = esp_netif_new(&netif_ppp_config);
+    esp_netif_t*           lte_netif        = esp_netif_new(&netif_ppp_config);
 
     if (lte_netif == NULL) {
         return -1;
@@ -67,14 +60,63 @@ int app_netif_lte_init(void) {
     dte_config.uart_config.rx_buffer_size   = 1024;
     dte_config.uart_config.tx_buffer_size   = 1024;
     dte_config.uart_config.event_queue_size = 4;
-    dte_config.task_stack_size              = 2048;
-    dte_config.task_priority                = 3;
+    dte_config.task_stack_size              = 1024;
+    dte_config.task_priority                = 2;
     dte_config.dte_buffer_size              = 512;
 
-    esp_modem_dce_t *dce = esp_modem_new(&dte_config, &dce_config, lte_netif);
+    esp_modem_dce_t* dce = esp_modem_new(&dte_config, &dce_config, lte_netif);
     if (dce == NULL) {
+        ESP_LOGE(LOG_TAG, "Failed to create modem.");
         return -2;
     }
+
+    if (xTaskCreate(app_netif_lte_task, "LTE", 2048, dce, 3, NULL) != pdPASS) {
+        ESP_LOGE(LOG_TAG, "Failed to create LTE task.");
+        return -3;
+    }
+
+    ESP_LOGI(LOG_TAG, "Initialization completed.");
+
+    return 0;
+}
+
+static void app_netif_lte_pin_init(void) {
+    gpio_config_t pin_conf = {
+        .pin_bit_mask = (1U << LTE_EN_PIN),
+        .intr_type    = GPIO_INTR_DISABLE,
+        .mode         = GPIO_MODE_OUTPUT_OD,
+        .pull_up_en   = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    };
+
+    gpio_config(&pin_conf);
+
+    pin_conf.pin_bit_mask = 1U << LTE_DTR_PIN;
+    pin_conf.mode         = GPIO_MODE_OUTPUT_OD;
+
+    gpio_config(&pin_conf);
+
+    gpio_set_level(LTE_DTR_PIN, 1U);
+}
+
+static void app_netif_lte_reset(void) {
+    gpio_set_level(LTE_EN_PIN, 0U);
+    vTaskDelay(pdMS_TO_TICKS(100));
+    gpio_set_level(LTE_EN_PIN, 1U);
+    vTaskDelay(pdMS_TO_TICKS(100));
+}
+
+/* TODO: Find a way to detect PPP status and reconnect */
+
+static void app_netif_lte_ppp_event_cb(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    ESP_LOGI(LOG_TAG, "PPP event ID received: %ld", event_id);
+    if (xQueueSend(s_lte_ppp_event_queue, &event_id, 0) != pdPASS) {
+        ESP_LOGW(LOG_TAG, "PPP event lost...");
+    }
+}
+
+static void app_netif_lte_task(void* arguments) {
+    esp_modem_dce_t* dce = arguments;
 
     char resp_buf[32];
     while (esp_modem_get_module_name(dce, resp_buf) != ESP_OK) {
@@ -95,35 +137,9 @@ int app_netif_lte_init(void) {
     esp_err_t ret = esp_modem_set_mode(dce, ESP_MODEM_MODE_DATA);
     if (ret != ESP_OK) {
         ESP_LOGE(LOG_TAG, "Set module to data mode failed: %d", ret);
-        return -4;
     }
 
-    ESP_LOGI(LOG_TAG, "Initialization completed.");
-
-    return 0;
-}
-
-static void app_netif_lte_reset(void) {
-    app_netif_lte_disable();
-    vTaskDelay(pdMS_TO_TICKS(100));
-    app_netif_lte_enable();
-    vTaskDelay(pdMS_TO_TICKS(100));
-}
-
-static void app_netif_lte_enable(void) {
-    gpio_set_level(LTE_EN_PIN, 1U);
-}
-
-static void app_netif_lte_disable(void) {
-    gpio_set_level(LTE_EN_PIN, 0U);
-}
-
-/* TODO: Find a way to detect PPP status and reconnect */
-
-static void app_netif_lte_ppp_event_cb(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    ESP_LOGI(LOG_TAG, "PPP state changed event %" PRIu32, event_id);
-    if (event_id == NETIF_PPP_ERRORUSER) {
-        esp_netif_t **p_netif = event_data;
-        ESP_LOGI(LOG_TAG, "User interrupted event from netif: %p", *p_netif);
+    for (;;) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
