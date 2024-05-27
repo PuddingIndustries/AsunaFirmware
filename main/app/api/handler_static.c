31af5b7b644554a50d58cf583ea2ed262d8fcf5c
Yilin Sun <imi415@imi.moe>
2024-05-27 23:31:14 +0800

    Added basic MIME types.
    Signed-off-by: Yilin Sun <imi415@imi.moe>

--- main/app/api/handler_static.c
+++ main/app/api/handler_static.c
@@ -8,6 +8,17 @@
 /* App */
 #include "app/api/handler_static.h"
 
+typedef struct {
+    char *file_ext;
+    char *mime_type;
+} app_mime_type_t;
+
+static const app_mime_type_t s_app_mime_types[] = {
+    {.file_ext = "html", .mime_type = "text/html"},
+    {.file_ext = "css", .mime_type = "text/css"},
+    {.file_ext = "js", .mime_type = "text/javascript"},
+};
+
 static esp_err_t app_api_handler_static_get(httpd_req_t *req) {
     return ESP_OK;
 }
